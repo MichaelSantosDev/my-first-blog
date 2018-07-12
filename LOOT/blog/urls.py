673d8de5cb992cc4d760e891bf48740ce83dbda4
unknown <neomcl@gmail.com>
2018-07-12 01:20:03 +0100

    Added form post

--- LOOT/blog/urls.py
+++ LOOT/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import post_list
 from .views import post_detail
+from .views import post_new
 
 
 
@@ -8,5 +9,5 @@ from .views import post_detail
 urlpatterns = [
     path('post_list/', post_list, name="post_list"),
     path('post/<int:pk>/', post_detail, name='post_detail'),
-
+    path('post/new', post_new, name='post_new'),
 ]
